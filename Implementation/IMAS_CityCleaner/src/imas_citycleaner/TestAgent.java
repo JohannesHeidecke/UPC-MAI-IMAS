a7323ceac746ac47404017da6e9951206da62f1e
Johannes Heidecke <jheidecke@gmail.com>
2016-11-07 16:08:35 +0100

    Initial Jade Project in Netbeans

---
+++ Implementation/IMAS_CityCleaner/src/imas_citycleaner/TestAgent.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imas_citycleaner;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author Ihcrul
+ */
+public class TestAgent extends Agent {
+
+    protected void setup() {
+        System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
+    }
+
+}
